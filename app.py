c128406aeb136ca4116abcfb998140369c1bd875
Cheena8 <111250847+Cheena8@users.noreply.github.com>
2022-11-14 11:14:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from joblib import load
+
+app = Flask(__name__)
+model_path = "svm.joblib"
+model = load(model_path)
+
+@app.route("/")
+def hello_world():
+    return "<b>Hello, World!</b>"
+
+@app.route("/sum", methods=["POST"])
+def sum():
+    print(request.json)
+    x=request.json['x']
+    y=request.json['y']
+    z=x+y
+    return jsonify({'sum':z})
+@app.route("/predict", methods=['POST'])
+def predict_digit():
+    image = request.json['image']
+    print("done loading")
+    predicted = model.predict([image])
+    return {"y_predicted":int(predicted[0])}
